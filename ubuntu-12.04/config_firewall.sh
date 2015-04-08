b3c055935a33b016ed29a238dc0e88d86e327c0b
Palash Nandy <palashn@gmail.com>
2015-04-08 10:28:04 -0400

    added mit mirrors.

--- ubuntu-12.04/config_firewall.sh
+++ ubuntu-12.04/config_firewall.sh
@@ -11,11 +11,12 @@ then
     exit 1
 fi
 
-ufw enable
+
 ufw logging on
 ufw default allow incoming
 ufw allow ssh
-ufw allow from 18.85.0.0/16
+ufw allow from 18.0.0.0/8
 ufw allow from 192.168.0.0/16
 ufw allow from 10.0.0.0/8
 ufw deny from any
+ufw enable
--- ubuntu-12.04/initialize.sh
+++ ubuntu-12.04/initialize.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+# Script to bring up an ubuntu-12.04 machine to prg standard
+# @palash
+#
+
 if [ "$(id -u)" != "0" ]
 then
     echo ""
@@ -7,8 +11,16 @@ then
     exit 1
 fi
 
+# switch to MIT mirrors for faster install
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
+sed 's@http://us\.archive\.ubuntu\.com/@http://mirrors.mit.edu/@' -i /etc/apt/sources.list
+
+
+# ROS setup
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | apt-key add -
+
+# refresh cache
 apt-get -y update
 apt-get -y install ros-hydro-desktop-full
 rosdep init
